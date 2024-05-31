d48bef0e46dbeb98e6d2ecea11cd473ef457544d
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-05-31 21:04:13 +0900

    Refactor: 코드 개선

--- src/pages/counselor/CounselorPage.jsx
+++ src/pages/counselor/CounselorPage.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import React from 'react';
+import { useNavigate} from 'react-router-dom';
 import {useStatus} from './hooks/useStatus'
 import { useRoomList } from './hooks/useRoomList';
 import { useCounselor } from './hooks/useCounselor';
